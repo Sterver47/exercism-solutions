029e56ca673eb7739dff16bb63c78c28a2974c99
Stefan Uram <sterver12345@gmail.com>
2022-02-05 16:38:35 +0100

    Go: Hello World added

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting contains function “HelloWorld" which greets the World
+package greeting
+
+// HelloWorld simply returns string "Hello, World!".
+func HelloWorld() string {
+	return "Hello, World!"
+}
