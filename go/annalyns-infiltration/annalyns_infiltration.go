5d0ac2c16555d7c71a82bc2a2eeff7af2134cd7d
Stefan Uram <sterver12345@gmail.com>
2022-02-06 18:03:14 +0100

    Go: Annalyn's Infiltration added

---
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,23 @@
+// Package annalyns implements functions fot a quest logic.
+package annalyn
+
+// CanFastAttack returns if FastAttack can be executed.
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+// CanSpy returns if Spy can be executed.
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+}
+
+// CanSignalPrisoner returns if SignalPrisoner can be executed.
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	return prisonerIsAwake && !archerIsAwake
+}
+
+// CanFreePrisoner returns if FreePrisoner can be executed.
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	return (prisonerIsAwake && !(knightIsAwake || archerIsAwake)) ||
+		(petDogIsPresent && !archerIsAwake)
+}
