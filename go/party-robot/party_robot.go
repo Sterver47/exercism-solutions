4f28734ae556149c4884b232c76e5c9c2d1c6608
Stefan Uram <sterver12345@gmail.com>
2022-02-06 18:10:21 +0100

    Go: Party Robot added

---
+++ go/party-robot/party_robot.go
@@ -0,0 +1,27 @@
+// Package partyrobot implements functions that the party robot use.
+package partyrobot
+
+import "fmt"
+
+// Welcome greets a person by name.
+func Welcome(name string) string {
+	return fmt.Sprintf("Welcome to my party, %s!", name)
+}
+
+// HappyBirthday wishes happy birthday to the birthday person and stands out his age.
+func HappyBirthday(name string, age int) string {
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+}
+
+// AssignTable assigns a table to each guest.
+func AssignTable(
+	name string,
+	table int,
+	neighbour string,
+	direction string,
+	distance float64,
+) string {
+	return fmt.Sprintf("Welcome to my party, %s!\n"+
+		"You have been assigned to table %X. Your table is %s, exactly %.1f meters from here.\n"+
+		"You will be sitting next to %s.", name, table, direction, distance, neighbour)
+}
