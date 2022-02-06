837cb2b9b1e5957e18fe56f9a4376bd95aa9516d
Stefan Uram <sterver12345@gmail.com>
2022-02-06 18:33:34 +0100

    Go: Need For Speed added

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,36 @@
+// Package speed implements functions for car racing.
+package speed
+
+type Car struct {
+	battery, batteryDrain, speed, distance int
+}
+
+type Track struct {
+	distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{speed: speed, batteryDrain: batteryDrain, battery: 100, distance: 0}
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery > car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	carMaxDistance := car.distance + car.battery/car.batteryDrain*car.speed
+	return carMaxDistance > track.distance
+}
