880f6fb3f167d836dafaf8e3d844fe35325f626c
Stefan Uram <sterver12345@gmail.com>
2022-02-13 20:32:18 +0100

    Go: Vehicle Purchase added

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,34 @@
+// Package purchase implements functions to help prepare to buy a vehicle
+package purchase
+
+import (
+	"fmt"
+	"strings"
+)
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
+func ChooseVehicle(option1, option2 string) string {
+	if strings.Compare(option1, option2) == 1 {
+		option1 = option2
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", option1)
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	switch {
+	case age < 3:
+		return originalPrice * 0.8
+	case age < 10:
+		return originalPrice * 0.7
+	}
+	return originalPrice * 0.5
+}
