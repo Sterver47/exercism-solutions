049449fc7e68869664ee799233ddf655645dedd7
Stefan Uram <sterver12345@gmail.com>
2022-04-16 20:35:01 +0200

    Go: Animal Magic added

---
+++ go/animal-magic/animal_magic.go
@@ -0,0 +1,33 @@
+// Package chance implements a few functions for a new children's game.
+package chance
+
+import (
+	"math/rand"
+	"time"
+)
+
+// SeedWithTime seeds math/rand with the current computer time.
+func SeedWithTime() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	sides := 20
+	return rand.Intn(sides) + 1
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	maxEnergy := 12.0
+	return rand.Float64() * maxEnergy
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+	return animals
+}
