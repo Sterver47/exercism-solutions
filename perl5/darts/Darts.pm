a640561c36a1473178fc37fb7c655a342f5c509c
Stefan Uram <sterver12345@gmail.com>
2023-09-23 21:34:37 +0200

    Perl: Darts small change #12in23

--- perl5/darts/Darts.pm
+++ perl5/darts/Darts.pm
@@ -6,8 +6,7 @@ use Exporter qw<import>;
 our @EXPORT_OK = qw<score_dart>;
 
 sub score_dart {
-    my ($x, $y) = @_;
-    my $distance = sqrt( $x**2 + $y**2 );
+    my $distance = sqrt( @_[0]**2 + @_[1]**2 );
     return 10 if $distance <= 1;
     return 5  if $distance <= 5;
     return 1  if $distance <= 10;
