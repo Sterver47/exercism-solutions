5d0ed1a4058a115719bf3ecebadafd0a63c0db95
Stefan Uram <sterver12345@gmail.com>
2023-10-04 00:02:37 +0200

    Crystal: Clock updated #12in23

--- crystal/clock/src/clock.cr
+++ crystal/clock/src/clock.cr
@@ -1,29 +1,21 @@
 class Clock
-  @total_minutes : Int32
+  protected getter(total_minutes : Int32)
 
   def initialize(hour : Int32 = 0, minute : Int32 = 0) : Nil
-    @total_minutes = minute + hour * 60
-  end
-
-  def total_minutes : Int32
-    @total_minutes
+    @total_minutes = (minute + hour * 60) % 1440
   end
 
   def to_s : String
-    sprintf("%02d:%02d", (@total_minutes // 60) % 24, @total_minutes % 60)
+    "%02d:%02d" % [(@total_minutes // 60) % 24, @total_minutes % 60]
   end
 
-  def +(other : Clock) : Clock
-    @total_minutes += other.total_minutes
-    self
-  end
-
-  def -(other : Clock) : Clock
-    @total_minutes -= other.total_minutes
-    self
-  end
+  {% for name in %w{+ -} %}
+    def {{name.id}}(other : Clock) : Clock
+      self.class.new(minute: (total_minutes {{name.id}} other.total_minutes) % 1440)
+    end
+  {% end %}
 
   def ==(other : Clock) : Bool
-    (self.total_minutes % 1440) == (other.total_minutes % 1440)
+    self.total_minutes == other.total_minutes
   end
 end
