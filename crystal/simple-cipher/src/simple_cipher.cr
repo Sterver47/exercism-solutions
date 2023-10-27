bd132e7d246ccde1bf5d05112f516f60ad24e89e
Stefan Uram <sterver12345@gmail.com>
2023-10-27 22:41:59 +0200

    Crystal: Simple Sipher added #12in23

---
+++ crystal/simple-cipher/src/simple_cipher.cr
@@ -0,0 +1,23 @@
+class SimpleCipher
+  getter key : String
+
+  def initialize : Nil
+    @key = 100.times.map { 'a' + rand(26) }.join
+  end
+
+  def initialize(@key : String) : Nil
+    if @key.size == 0 || @key =~ /[^a-z]/
+      raise ArgumentError.new("Key must be all lowercase letters")
+    end
+  end
+
+  {% for methodname, operator in {encode: '+', decode: '-'} %}
+  def {{methodname}}(text : String) : String
+    String.build do |result|
+      text.each_char.zip(@key.each_char.cycle) do |char, key_char|
+        result << ('a'.ord + ((char - 'a') {{operator.id}} (key_char - 'a')) % 26).chr
+      end
+    end
+  end
+  {% end %}
+end
