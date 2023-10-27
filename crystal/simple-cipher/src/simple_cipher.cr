6adfd0165261528ca0ba4c6a9f36747f9bba2e64
Stefan Uram <sterver12345@gmail.com>
2023-10-27 23:34:56 +0200

    Crystal: Simple Cipher updated #12in23

--- crystal/simple-cipher/src/simple_cipher.cr
+++ crystal/simple-cipher/src/simple_cipher.cr
@@ -1,23 +1,15 @@
 class SimpleCipher
   getter key : String
 
-  def initialize : Nil
-    @key = 100.times.map { 'a' + rand(26) }.join
-  end
-
-  def initialize(@key : String) : Nil
-    if @key.empty? || @key =~ /[^a-z]/
-      raise ArgumentError.new("Key must be all lowercase letters")
-    end
+  def initialize(@key : String = "") : Nil
+    @key = 100.times.map { 'a' + rand(26) }.join if @key.empty?
   end
 
   {% for methodname, operator in {encode: '+', decode: '-'} %}
   def {{methodname}}(text : String) : String
-    String.build do |result|
-      text.each_char.zip(@key.each_char.cycle) do |char, key_char|
-        result << ('a'.ord + ((char - 'a') {{operator.id}} (key_char - 'a')) % 26).chr
-      end
-    end
+      text.each_char.zip(@key.each_char.cycle).map do |char, key_char|
+       ('a'.ord + ((char - 'a') {{operator.id}} (key_char - 'a')) % 26).chr
+    end.join
   end
   {% end %}
 end
